f5ca12482a07859d155e8ad97ac99d855b23d068
Ahmed Ayad <ahmedayad@Ahmeds-MacBook-Pro.local>
2020-07-04 14:42:16 +0300

    adding actor page

--- ahmed-yara-sara/src/App.js
+++ ahmed-yara-sara/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import FooterBar from "./components/Footer";
 import MoviePage from "./components/MoviePage";
+import ActorPage from "./components/ActorPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App(props) {
   const [movies, setMovies] = useState([]);
@@ -13,9 +14,11 @@ function App(props) {
       <Navbar handleMovies={setMovies} />
       <Router>
         <Switch>
-          <Route path="/" exact>
-            <Main movies={movies} />
-          </Route>
+       
+        
+          <Route path="/" exact component={ActorPage}/>
+          
+       
           <Route path="/Movie/:id" component={MoviePage}></Route>
         </Switch>
       </Router>
--- ahmed-yara-sara/src/components/ActorPage.js
+++ ahmed-yara-sara/src/components/ActorPage.js
@@ -19,7 +19,7 @@ const TMDB_BASE_URL = "https://api.themoviedb.org/3";
   }
     
       useEffect(async()=>{
-        URL = constructUrl(`/person/${person_id}`);
+        URL = constructUrl(`person/${person_id}`);
         let res= await fetch(URL)
         let data=await res.json();
         setActor(data);
@@ -29,14 +29,24 @@ const TMDB_BASE_URL = "https://api.themoviedb.org/3";
       
      
       console.log(actor);
-      
+      let image=`https://image.tmdb.org/t/p/w500/${actor.profile_path}`
 
   return (
-    <>
-        <div>
-           
-            <h1>just</h1>
+    
+        <div className="container">
+                <div className="row">
+            <div className="col-4">
+              <img src={image} width="100%"/>
+           </div>
+  <div className="col-8">
+    <div><strong> {actor.name}</strong></div>
+    <div><strong> {actor.birthday}</strong></div>
+    <div> {actor.biography}</div>
+  </div>
+          </div>
         </div>
-   </>
+         
+       
+   
   );
 }
