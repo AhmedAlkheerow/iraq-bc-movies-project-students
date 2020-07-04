f36bc49c5a90f066640940b1e28b3c0ff5e48b81
Yara Alkassar <yara.sarmad@yahoo.com>
2020-07-04 23:52:22 +0300

    formatted documents

--- ahmed-yara-sara/src/components/Image.js
+++ ahmed-yara-sara/src/components/Image.js
@@ -1,22 +1,20 @@
-import React, {useEffect } from "react";
-import MovieGrid from "./MovieGrid";
-import MoviePage from "./MoviePage";
-import { Card, Button, Col } from "react-bootstrap";
-
-
+import React, { useEffect } from "react";
+import { Card } from "react-bootstrap";
 function Image(props) {
-    let image=`https://image.tmdb.org/t/p/w500/${props.image}`
-      let  style={
-            height:"500px",
-            width:"400px",
-            margin:"4px"
-        }
+  let image = `https://image.tmdb.org/t/p/w500/${props.image}`;
+  let style = {
+    height: "500px",
+    width: "400px",
+    margin: "4px",
+  };
   return (
     <>
-    <card ><a href="#">
-    <Card.Img variant="top" src={image} style={style}/></a>
-    </card>
-      </>
+      <card>
+        <a href="#">
+          <Card.Img variant="top" src={image} style={style} />
+        </a>
+      </card>
+    </>
   );
 }
 
