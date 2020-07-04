abfc91bd98cda080bf52d7007f3765c81e4dcde4
Yara Alkassar <yara.sarmad@yahoo.com>
2020-07-04 15:17:10 +0300

    Version 5 kind of done.

--- ahmed-yara-sara/src/components/ActorPage.js
+++ ahmed-yara-sara/src/components/ActorPage.js
@@ -1,53 +1,51 @@
 import React, { useState, useEffect } from "react";
 import Card, { CardBody } from "react-bootstrap/Card";
 import Search from "./Search";
-import {Button} from 'react-bootstrap';
-import {BrowserRouter as Router ,Switch ,Route,Link} from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Image from "./Image";
-import MovieDetails from "./MovieDetails"
-import {useHistory} from "react-router";
+import MovieDetails from "./MovieDetails";
+import { useHistory } from "react-router";
 import Production from "./Production";
 export default function Actors(props) {
-    const history = useHistory();
-   
-   const  person_id= props.match.params.actor_id;
-    const [actor,setActor]=useState("");
-const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+  const history = useHistory();
+
+  const person_id = props.match.params.actor_id;
+  const [actor, setActor] = useState("");
+  const TMDB_BASE_URL = "https://api.themoviedb.org/3";
   const constructUrl = (path, query) => {
     return `${TMDB_BASE_URL}/${path}?api_key=${atob(
       "ZDJmYTdhZDFlMjZhZjA4NDdkMzQ5ZDdkYmQ1ZjkzZTU="
     )}&query=${query}`;
-  }
-    
-      useEffect(async()=>{
-        URL = constructUrl(`person/${person_id}`);
-        let res= await fetch(URL)
-        let data=await res.json();
-        setActor(data);
-        console.log(data)
-        
-      },[person_id]);
-      
-     
-      console.log(actor);
-      let image=`https://image.tmdb.org/t/p/w500/${actor.profile_path}`
+  };
+
+  useEffect(async () => {
+    URL = constructUrl(`person/${person_id}`);
+    let res = await fetch(URL);
+    let data = await res.json();
+    setActor(data);
+    console.log(data);
+  }, [person_id]);
+
+  console.log(actor);
+  let image = `https://image.tmdb.org/t/p/w500/${actor.profile_path}`;
 
   return (
-    
-        <div className="container">
-                <div className="row">
-            <div className="col-4">
-              <img src={image} width="100%"/>
-           </div>
-  <div className="col-8">
-    <div><strong> {actor.name}</strong></div>
-    <div><strong> {actor.birthday}</strong></div>
-    <div> {actor.biography}</div>
-  </div>
-          </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <img src={image} width="100%" />
         </div>
-         
-       
-   
+        <div className="col-8">
+          <div>
+            <strong> {actor.name}</strong>
+          </div>
+          <div>
+            <strong> {actor.birthday}</strong>
+          </div>
+          <div> {actor.biography}</div>
+        </div>
+      </div>
+    </div>
   );
 }
--- ahmed-yara-sara/src/components/Actors.js
+++ ahmed-yara-sara/src/components/Actors.js
@@ -1,59 +1,60 @@
-
 import { Button, Modal } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import Card, { CardBody } from "react-bootstrap/Card";
 import Search from "./Search";
-import {BrowserRouter as Router ,Switch ,Route,Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Image from "./Image";
-import MovieDetails from "./MovieDetails"
-import {useHistory} from "react-router";
+import MovieDetails from "./MovieDetails";
+import { useHistory } from "react-router";
 import Production from "./Production";
 export default function Actors(props) {
-   const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const actors = Object.values(props.details);
-    const history = useHistory();
-    const [actor,setActor]=useState("");
-const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+  const history = useHistory();
+  const [actor, setActor] = useState("");
+  const TMDB_BASE_URL = "https://api.themoviedb.org/3";
   const constructUrl = (path, query) => {
     return `${TMDB_BASE_URL}/${path}?api_key=${atob(
       "ZDJmYTdhZDFlMjZhZjA4NDdkMzQ5ZDdkYmQ1ZjkzZTU="
     )}&query=${query}`;
-  }
-    
+  };
 
-      
-     
-      console.log(actor);
-      
+  console.log(actor);
 
   return (
     <>
-    <Button
-    variant="dark"
-    onClick={handleShow}
-    className="btn btn-dark my-5 mx-5"
-  >
-    Click to see Cast!
-  </Button>
-  <Modal show={show} onHide={handleClose}>
-    <Modal.Header closeButton>
-      <Modal.Title>Cast</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <ul>
-        {actors.map((actor) => {
-          return <li><a href={`/person/${actor.id}`}>{actor.name}</a></li>;
-        })}
-      </ul>
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="danger" onClick={handleClose}>
-        Close
+      <Button
+        variant="dark"
+        onClick={handleShow}
+        className="btn btn-dark my-5 mx-5"
+      >
+        Click to see Cast!
       </Button>
-    </Modal.Footer>
-  </Modal>
-</>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cast</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul>
+            {actors.map((actor) => {
+              return (
+                <li>
+                  <a href={`/person/${actor.id}`} style={{ color: "black" }}>
+                    {actor.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
